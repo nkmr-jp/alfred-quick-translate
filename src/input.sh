4e551decf7b091f59ea6bd7cad122d3a94a681a4
nkmr-jp <nkmr.git@gmail.com>
2022-01-21 11:10:38 +0900

    :sparkles: Add feature that translate multi lines comments in code.

--- src/input.sh
+++ src/input.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 # shellcheck disable=SC2154
 input=$(echo "$input" | tr '\n' ';')
-input=$(echo "$input" | sed 's/\. /. ;/g')
-input=$(echo "$input" | sed 's/\。/。;/g')
+input=${input//    /}
+input=${input//\. /. ;}
+input=${input//\。/。;}
+
 if [[ $input =~ 。 ]]; then
-  input=$(echo "$input" | sed 's/;;/;/g')
+  input=${input//;;/;}
 fi
+
+# // comment e.g. golang
+input=${input//;\/\/ / }
+input=${input//\/\/ /}
+
+# # comment e.g. python
+input=${input//;\# / }
+input=${input//\# /}
 
 echo -n "$input"
